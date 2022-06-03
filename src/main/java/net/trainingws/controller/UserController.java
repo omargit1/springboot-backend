eb497c346476658f58884440278fb369dbbe934c
oelassri <elassri.omar@gmail.com>
2022-06-03 14:22:22 +0200

    first commit , add userController list, create

---
+++ src/main/java/net/trainingws/controller/UserController.java
@@ -0,0 +1,33 @@
+package net.trainingws.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.trainingws.model.User;
+import net.trainingws.repository.UserRepository;
+
+@CrossOrigin( origins = "http://localhost:4200")
+@RestController
+@RequestMapping("api/")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("users")
+	public List<User> getUsers() {
+		return this.userRepository.findAll();
+	}
+	
+	@PostMapping("add")
+	public User addUser(@RequestBody User user) {
+		return this.userRepository.save(user);
+	}
+}
